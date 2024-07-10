2934467911e84586241b3091f4039c7e9f8b137e
Tran Vu Binh <tranb4010@gmail.com>
2024-07-10 10:51:29 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+import Banner from './components/Banner'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div>
+    <div className="p-4 flex justify-between  fixed top-0 left-0 w-full z-[9999]  bg-black">
+        <div className="flex items-center space-x-4">
+            <h1 className="text-[40px] text-red-700 uppercase font-bold">PHIM VỊT</h1>
+            <nav className="flex items-center space-x-4">
+                <a href="" className="text-white">Home</a>
+                <a href="" className="text-white">About</a>
+                <a href="" className="text-white">Contact</a>
+            </nav>
+        </div>
+        <div className="flex items-center space-x-4">
+            <input type="text" placeholder="Search" className="p-2 rounded-md text-black" />
+            <button className="bg-red-700 text-white px-4 py-2 rounded-md">Search</button>
+        </div>
+        
+    </div>
+    </div>  );
+};
+
+export default Header;
